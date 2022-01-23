33f50865194f9b9d61c9fb3743c38e9cb5f0f62b
atjhamit <atjhamit@gmail.com>
2022-01-23 01:25:22 +0000

    lib file creation for common import

--- udemy/recursion/generic/sumOfNaturalNos.cpp
+++ udemy/recursion/generic/sumOfNaturalNos.cpp
@@ -1,1 +1,15 @@
-#include
+#include "../../lib/commonImport.hpp"
+
+int sum(int n)
+{
+    if (n < 1) return 0;
+
+    return n + sum(n-1);
+}
+
+int main()
+{
+    int val = 5;
+    LOG("sum of first 5 natural nos : " << sum(val));
+    return 0;
+}
